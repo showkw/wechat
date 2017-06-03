247789049270d7ff2c2d5ca39d778e19df3b90e6
showkw <showkw@163.com>
2017-06-03 20:55:48 +0800

    '12234'

--- wxModel.php
+++ wxModel.php
@@ -258,15 +258,15 @@ EOT;
         if( $_SESSION['access_token'] && $diff < 7000 ){
             $sess = json_encode($_SESSION);
 
-            file_put_contents( './get.txt','SESSION_TOKEN:'.$sess.PHP_EOL, FILE_APPEND );
             return $_SESSION['access_token'];
         }else{
             $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$this->appId."&secret=".$this->appSecret;
             $res = $this->toCurl( $url );
             $access_token = json_decode( $res, true)['access_token'];
+            $sess = json_encode($_SESSION);
+            file_put_contents( './get.txt','SESSION_TOKEN:'.$sess.PHP_EOL, FILE_APPEND );
             $_SESSION['access_token'] = $access_token;
             $_SESSION['expire_time'] = time();
-            $sess = json_encode($_SESSION);
             file_put_contents( './get.txt','TOKEN:'.$_SESSION['access_token'].PHP_EOL.$sess.PHP_EOL, FILE_APPEND );
 
             return $_SESSION['access_token'];
