84205cd85288a5403951ff2609d3dd2370f43fb9
showkw <showkw@163.com>
2017-06-03 21:43:43 +0800

    '服务号居然没权限'

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ define("TOKEN", "showking");//自己定义的token 就是个通信的私钥
 require_once 'wxModel.php';
 
 //=========================
-//这里是正式公众号!
-$appId= 'wx0f57a95d244904a6' ;
-$appSecret = '90ef5c49bffe3750dfe5fb33caa5ae3b';
+////这里是正式公众号!
+//$appId= 'wx0f57a95d244904a6' ;
+//$appSecret = '90ef5c49bffe3750dfe5fb33caa5ae3b';
 
 //====================
 //测试使用配置
-//$appId= 'wxf344ebfe858e6669' ;
-//
-//$appSecret = 'a600083b67235751dde0f1452d4beeca'; //测试用的
+$appId= 'wx402a31a201a2c9b9' ;
+
+$appSecret = 'd4624c36b6795d1d99dcf0547af5443d'; //测试用的
 //====================
 
 $wechatObj = new wechatCallbackapiTest( $appId, $appSecret );
