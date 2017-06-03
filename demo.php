758ee6c03f8330ebc193d6e92f16863c271cfb5d
showkw <showkw@163.com>
2017-06-03 20:39:15 +0800

    'token'

--- demo.php
+++ demo.php
@@ -6,3 +6,15 @@
  * Date: 2017/6/2
  * Time: 15:25
  */
+include 'wxModel.php';
+
+$appId= 'wxf344ebfe858e6669' ;
+
+$appSecret = 'a600083b67235751dde0f1452d4beeca'; //测试用的
+
+$wechatObj = new wechatCallbackapiTest( $appId, $appSecret );
+
+//session_start();
+//$_SESSION['access_token'] = null;
+//$_SESSION['expire_time'] = null;
+$wechatObj->getAccessToken();
